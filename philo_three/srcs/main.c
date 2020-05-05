f392cd56ccb0861f076f4e2c4df8184f41fc57ec
FlavorlessQuark <mahthaki@gmail.com>
2020-05-05 03:49:34 -0700

    automated push

--- philo_three/srcs/main.c
+++ philo_three/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jjosephi <jjosephi@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/22 14:42:53 by jjosephi          #+#    #+#             */
-/*   Updated: 2020/05/05 02:54:51 by jjosephi         ###   ########.fr       */
+/*   Updated: 2020/05/05 03:48:39 by jjosephi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,23 @@
 
 int main(int argc, char **argv)
 {
-	sem_t *sem;
 	pthread_t thread;
 	int args[6];
 	int i;
+	int *death;
 	long time;
 
 	parser(argv, &args, argc);
-	sem = sem_open("SEM_TWO", O_CREAT , S_IRWXO, args[0]);
+	sem_open("SEM_TWO", O_CREAT , S_IRWXO, args[0]);
 	i = 0;
 	time = 0;
 	pthread_create(&thread, NULL,time_ct, &time);
-	sem_close(sem);
 	i = args[0];
-	args[5] = args[0];
+	death = args[0];
 	while (i > 0)
 	{
-		Spawn(args, &time, &args[0], &args[5]);
+		Spawn(args, &time, &args[0], &death);
 		i--;
 	}
-	while (args[5] > 0);
+	while (death > 0);
 }
